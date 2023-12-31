9336042138fe696a2833adbf1dc63199eab4b920
UMESH-042 <202151042@iiitvadodara.ac.in>
2024-01-01 05:20:16 +0530

    made little changes in comment section

--- lib/view/Network/comments.dart
+++ lib/view/Network/comments.dart
@@ -1,202 +1,3 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:gigs/view/Network/Network.dart';
-
-// class CommentBottomSheet extends StatefulWidget {
-//   final String postId;
-
-//   CommentBottomSheet({Key? key, required this.postId}) : super(key: key);
-
-//   @override
-//   _CommentBottomSheetState createState() => _CommentBottomSheetState();
-// }
-
-// class _CommentBottomSheetState extends State<CommentBottomSheet> {
-//   TextEditingController _commentController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             Text(
-//               'Add a comment',
-//               style: TextStyle(
-//                 fontSize: 18,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 8),
-//             TextField(
-//               controller: _commentController,
-//               decoration: InputDecoration(
-//                 hintText: 'Write your comment...',
-//               ),
-//             ),
-//             SizedBox(height: 16),
-//             ElevatedButton(
-//               onPressed: () {
-//                 _postComment();
-//               },
-//               child: Text('Post Comment'),
-//             ),
-//             SizedBox(height: 16),
-//             _buildCommentSection(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildCommentSection() {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.stretch,
-//       children: [
-//         Text(
-//           'Comments',
-//           style: TextStyle(
-//             fontSize: 18,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         SizedBox(height: 8),
-//         _buildCommentCount(),
-//         SizedBox(height: 8),
-//         _buildCommentList(),
-//       ],
-//     );
-//   }
-
-//   Widget _buildCommentCount() {
-//     return StreamBuilder(
-//       stream: FirebaseFirestore.instance
-//           .collection('comments')
-//           .where('postId', isEqualTo: widget.postId)
-//           .snapshots(),
-//       builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//         if (!snapshot.hasData) {
-//           return Text('Loading...');
-//         }
-
-//         int commentCount = snapshot.data!.docs.length;
-//         return Text(
-//           '$commentCount Comments',
-//           style: TextStyle(
-//             fontSize: 16,
-//           ),
-//         );
-//       },
-//     );
-//   }
-
-//   Widget _buildCommentList() {
-//     return StreamBuilder(
-//       stream: FirebaseFirestore.instance
-//           .collection('comments')
-//           .where('postId', isEqualTo: widget.postId)
-//           .snapshots(),
-//       builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//         if (!snapshot.hasData) {
-//           return Text('Loading...');
-//         }
-
-//         var comments = snapshot.data!.docs;
-
-//         //       return ListView.builder(
-//         //         shrinkWrap: true,
-//         //         itemCount: comments.length,
-//         //         itemBuilder: (context, index) {
-//         //           var comment = comments[index];
-//         //           var commentText = comment['text'];
-//         //           var commentUser = comment['userId'];
-
-//         //           return ListTile(
-//         //             title: Text(commentText),
-//         //             subtitle: Text('User: $commentUser'),
-//         //           );
-//         //         },
-//         //       );
-//         //     },
-//         //   );
-//         // }
-//         return ListView.builder(
-//           shrinkWrap: true,
-//           itemCount: comments.length,
-//           itemBuilder: (context, index) {
-//             var comment = comments[index];
-//             var commentText = comment['text'];
-//             var commentUser = comment['userId'];
-
-//             return Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 // Display username
-//                 FutureBuilder(
-//                   future: _getUsername(commentUser),
-//                   builder: (context, AsyncSnapshot<String> usernameSnapshot) {
-//                     if (usernameSnapshot.connectionState ==
-//                         ConnectionState.waiting) {
-//                       return Text('Loading...');
-//                     } else if (usernameSnapshot.hasError) {
-//                       return Text('Error loading username');
-//                     }
-
-//                     var username = usernameSnapshot.data ?? 'Unknown User';
-
-//                     return Text(
-//                       username,
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     );
-//                   },
-//                 ),
-//                 // Display comment text
-//                 Text(commentText),
-//                 SizedBox(height: 8),
-//               ],
-//             );
-//           },
-//         );
-//       },
-//     );
-//   }
-
-//   Future<void> _postComment() async {
-//     final FirebaseAuth auth = FirebaseAuth.instance;
-//     final User user = auth.currentUser!;
-//     final uid = user.uid;
-
-//     if (_commentController.text.isNotEmpty) {
-//       await FirebaseFirestore.instance.collection('comments').add({
-//         'postId': widget.postId,
-//         'userId': uid,
-//         'text': _commentController.text,
-//         'timestamp': FieldValue.serverTimestamp(),
-//       });
-
-//       _commentController.clear();
-//        // Clear the text field after posting a comment
-//     }
-//   }
-
-//   Future<String> _getUsername(String userId) async {
-//     var userDoc =
-//         await FirebaseFirestore.instance.collection('users').doc(userId).get();
-
-//     if (userDoc.exists) {
-//       return userDoc['name'];
-//     } else {
-//       return 'Unknown User';
-//     }
-//   }
-// }
-
-
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -234,19 +35,6 @@ class _CommentBottomSheetState extends State<CommentBottomSheet> {
             SizedBox(height: 8),
             _buildCommentInputField(),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                _postComment();
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue, // Use a color that fits your theme
-              ),
-              child: Text(
-                'Post Comment',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 16),
             _buildCommentSection(),
           ],
         ),
@@ -255,16 +43,35 @@ class _CommentBottomSheetState extends State<CommentBottomSheet> {
   }
 
   Widget _buildCommentInputField() {
-    return TextField(
-      controller: _commentController,
-      decoration: InputDecoration(
-        hintText: 'Write your comment...',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.grey[200],
       ),
-      maxLines: 3,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _commentController,
+                decoration: InputDecoration(
+                  hintText: 'Write your comment...',
+                  border: InputBorder.none,
+                ),
+                maxLines: 3,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {
+                _postComment();
+              },
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -309,117 +116,123 @@ class _CommentBottomSheetState extends State<CommentBottomSheet> {
     );
   }
 
-Color _generateRandomColor(String input) {
-  final random = Random(input.hashCode);
+  Color _generateRandomColor(String input) {
+    final random = Random(input.hashCode);
 
-  // Generate dark color with a minimum brightness
-  Color generateDarkColor() {
-    return Color.fromARGB(
-      255,
-      random.nextInt(100) + 50, // Adjust the minimum brightness here
-      random.nextInt(100) + 50, // Adjust the minimum brightness here
-      random.nextInt(100) + 50, // Adjust the minimum brightness here
+    // Generate dark color with a minimum brightness
+    Color generateDarkColor() {
+      return Color.fromARGB(
+        255,
+        random.nextInt(100) + 50, // Adjust the minimum brightness here
+        random.nextInt(100) + 50, // Adjust the minimum brightness here
+        random.nextInt(100) + 50, // Adjust the minimum brightness here
+      );
+    }
+
+    return generateDarkColor();
+  }
+
+  Widget _buildCommentList() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('comments')
+          .where('postId', isEqualTo: widget.postId)
+          .orderBy('timestamp', descending: false)
+          .snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading...');
+        }
+
+        var comments = snapshot.data!.docs;
+
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: comments.length,
+          itemBuilder: (context, index) {
+            var comment = comments[index];
+            var commentText = comment['text'];
+            var commentUser = comment['userId'];
+            var timestamp = comment['timestamp'] as Timestamp;
+            Color randomColor = _generateRandomColor(commentUser);
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: FutureBuilder(
+                    future: _getUsername(commentUser),
+                    builder: (context, AsyncSnapshot<String> usernameSnapshot) {
+                      if (usernameSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return CircleAvatar(child: Text(''));
+                      } else if (usernameSnapshot.hasError) {
+                        return CircleAvatar(child: Text(''));
+                      }
+
+                      var username = usernameSnapshot.data ?? 'Unknown User';
+
+                      return CircleAvatar(
+                        backgroundColor: randomColor,
+                        child: Text(
+                          username[0],
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    },
+                  ),
+                  title: FutureBuilder(
+                    future: _getUsername(commentUser),
+                    builder: (context, AsyncSnapshot<String> usernameSnapshot) {
+                      if (usernameSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return Text('Loading...');
+                      } else if (usernameSnapshot.hasError) {
+                        return Text('Error loading username');
+                      }
+
+                      var username = usernameSnapshot.data ?? 'Unknown User';
+
+                      return Text(
+                        username,
+                        style: TextStyle(fontSize: 14),
+                        // style: TextStyle(
+                        //   fontWeight: FontWeight.bold,
+                        // ),
+                      );
+                    },
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        commentText,
+                        style: TextStyle(fontWeight: FontWeight.w900),
+                      ),
+                    ],
+                  ),
+                  trailing: Text(
+                    _formatTimestamp(timestamp),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                Divider(thickness: 1, color: Colors.grey),
+                SizedBox(height: 8),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 
-  return generateDarkColor();
-}
-
-Widget _buildCommentList() {
-  return StreamBuilder(
-    stream: FirebaseFirestore.instance
-        .collection('comments')
-        .where('postId', isEqualTo: widget.postId)
-        .orderBy('timestamp', descending: false)
-        .snapshots(),
-    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-      if (!snapshot.hasData) {
-        return Text('Loading...');
-      }
-
-      var comments = snapshot.data!.docs;
-
-      return ListView.builder(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: comments.length,
-        itemBuilder: (context, index) {
-          var comment = comments[index];
-          var commentText = comment['text'];
-          var commentUser = comment['userId'];
-          var timestamp = comment['timestamp'] as Timestamp;
-          Color randomColor = _generateRandomColor(commentUser);
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                leading: FutureBuilder(
-                  future: _getUsername(commentUser),
-                  builder: (context, AsyncSnapshot<String> usernameSnapshot) {
-                    if (usernameSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return CircleAvatar(child: Text(''));
-                    } else if (usernameSnapshot.hasError) {
-                      return CircleAvatar(child: Text(''));
-                    }
-
-                    var username = usernameSnapshot.data ?? 'Unknown User';
-
-                    return CircleAvatar(
-                      backgroundColor: randomColor,
-                      child: Text(username[0],style: TextStyle(color: Colors.white),),
-                    );
-                  },
-                ),
-                title: FutureBuilder(
-                  future: _getUsername(commentUser),
-                  builder: (context, AsyncSnapshot<String> usernameSnapshot) {
-                    if (usernameSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return Text('Loading...');
-                    } else if (usernameSnapshot.hasError) {
-                      return Text('Error loading username');
-                    }
-
-                    var username = usernameSnapshot.data ?? 'Unknown User';
-
-                    return Text(
-                      username,
-                      style: TextStyle(fontSize: 14),
-                      // style: TextStyle(
-                      //   fontWeight: FontWeight.bold,
-                      // ),
-                    );
-                  },
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(commentText,style: TextStyle(fontWeight: FontWeight.w900),),
-                  ],
-                ),
-                trailing: Text(
-                  _formatTimestamp(timestamp),
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-              Divider(thickness: 1, color: Colors.grey),
-              SizedBox(height: 8),
-            ],
-          );
-        },
-      );
-    },
-  );
-}
-
-String _formatTimestamp(Timestamp timestamp) {
-  var timeAgo = timeago.format(
-    timestamp.toDate(),
-    allowFromNow: true,
-  );
-  return timeAgo;
-}
+  String _formatTimestamp(Timestamp timestamp) {
+    var timeAgo = timeago.format(
+      timestamp.toDate(),
+      allowFromNow: true,
+    );
+    return timeAgo;
+  }
 
   Future<void> _postComment() async {
     final FirebaseAuth auth = FirebaseAuth.instance;
